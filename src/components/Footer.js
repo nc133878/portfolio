7e632e0985fb0200cef379d081d6711bceb7e3f6
nc133878 <crawford.neal18@gmail.com>
2024-07-29 19:04:20 -0400

    Updated font colors, added, resume button, adjust image heights

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Footer(){
     return(
         <footer>
             <a target="_blank" rel="noreferrer" href="https://github.com/nc133878" ><img src='../images/github-logo-grey.png' alt="git logo" /></a>
+            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1lMqUONh6siCC1IPH41cGQ9XRJZC9mIHG/view?usp=sharing" ><FontAwesomeIcon className='icon-grey' icon={faFileAlt} size='2x' /></a>
             <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/neal-crawford-a4816085/" ><img src='../images/linkedin-logo-grey.png ' alt="linkedIn logo" /></a>
         </footer>
     )
