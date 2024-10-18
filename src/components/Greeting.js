73a2d29f494c748909896f907ac4bf1ec4292200
nc133878 <crawford.neal18@gmail.com>
2024-10-17 21:04:20 -0400

    Update Greeting.js

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,7 +4,7 @@ function Greeting() {
   return (
     <section className='greeting'>
         <div>
-            <p>Hello there! <br /> my name is <span className='greeting-name'>NEAL</span></p>
+            <p>Hello there! <br /> my name is <span className='greeting-name'></span></p>
         </div>
     </section>
   )
