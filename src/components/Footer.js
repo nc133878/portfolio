f019994db60326eeb4c68c86eb6734c9c6ca35ca
nc133878 <crawford.neal18@gmail.com>
2025-08-28 09:05:21 -0400

    Added REsume to file sysytem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer(){
     return(
         <footer>
             <a target="_blank" rel="noreferrer" href="https://github.com/nc133878" ><img src='../images/github-logo-grey.png' alt="git logo" /></a>
-            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1lMqUONh6siCC1IPH41cGQ9XRJZC9mIHG/view?usp=sharing" ><FontAwesomeIcon className='icon-grey' icon={faFileAlt} size='2x' /></a>
+            <a target="_blank" rel="noreferrer" href="./assets/Neal_Crawford_Resume_25.pdf" ><FontAwesomeIcon className='icon-grey' icon={faFileAlt} size='2x' /></a>
             <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/neal-crawford-a4816085/" ><img src='../images/linkedin-logo-grey.png ' alt="linkedIn logo" /></a>
         </footer>
     )
