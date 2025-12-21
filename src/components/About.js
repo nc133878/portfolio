25b2fe56c4d4321d939724f69859a4bd50f6b023
nc133878 <crawford.neal18@gmail.com>
2025-12-20 22:27:32 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import React from 'react';
     return(
         <section className='about-section'>
             <h2 class='center'>About</h2>
-            <p> As a seasoned developer with over 6+ years of experience, I have a proven track record of delivering high-quality, scalable, and efficient applications. I have a particular interest in developing costumer facing solutions using frontend frameworks.  
+            <p> As a seasoned developer with over 7+ years of experience, I have a proven track record of delivering high-quality, scalable, and efficient applications. I have a particular interest in developing costumer facing solutions using frontend frameworks.  
                 I enjoy working on enthusiastic teams that stimulate innovation and creative thinking.</p>
             <h2 className='center'>Interests</h2>
             <p> Rhythm & Blues connoissuer. Songwriting novice. Physical Fitness addict. 
