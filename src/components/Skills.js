6fef83798c2ae84e53c1b92f3d048ddbb36d8b22
nc133878 <crawford.neal18@gmail.com>
2026-03-20 02:07:49 -0400

    Layout updates; added skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,14 +5,16 @@ export default function Skills(){
         <section className='skills-section'>
             <h2>Skills</h2>
             <div className='logo-grid'>
-                <img className='html-logo' src='./images/HTML5_Badge.png' alt="html-logo"/>
-                <img className='logo' src='./images/js-logo.png' alt="js logo"/>
-                <img className='css-logo' src='./images/css-logo.png' alt="css logo"/>
-                <img className='logo' src='./images/logo192.png' alt="react logo"/>
-                <img className='logo' src='./images/python-logo.png' alt="python logo"/>
-                <img className='logo' src='./images/git-logo.png' alt="git logo"/>
-                <img className='logo' src='./images/tailwind-logo.png' alt="tailwind logo"/>
-                <img className='logo' src='./images/nodejs.png' alt="node.js logo"/>
+                <img className='skill-logo' src='./images/HTML5_Badge.png' alt="HTML5"/>
+                <img className='skill-logo' src='./images/js-logo.png' alt="JavaScript"/>
+                <img className='skill-logo' src='./images/css-logo.png' alt="CSS"/>
+                <img className='skill-logo' src='./images/logo192.png' alt="React"/>
+                <img className='skill-logo' src='./images/python-logo.png' alt="Python"/>
+                <img className='skill-logo' src='./images/git-logo.png' alt="Git"/>
+                <img className='skill-logo' src='./images/tailwind-logo.png' alt="Tailwind CSS"/>
+                <img className='skill-logo' src='./images/nodejs.png' alt="Node.js"/>
+                <img className='skill-logo' src='./images/go-logo.png' alt="Go"/>
+                <img className='skill-logo' src='./images/postgresql.png' alt="PostgreSQL"/>
             </div>
         </section>
     )
