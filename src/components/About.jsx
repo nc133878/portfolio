8a9c6c9983ecccb8ef1c9e16f44fe7b45bd75197
nc133878 <crawford.neal18@gmail.com>
2026-06-12 02:20:56 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import useReveal from '../hooks/useReveal';
             <p> As a seasoned developer with over 7+ years of experience, I have a proven track record of delivering high-quality, scalable, and efficient applications. I have a particular interest in developing costumer facing solutions using frontend frameworks.  
                 I enjoy working on enthusiastic teams that stimulate innovation and creative thinking.</p>
             <h2 className='center'>Interests</h2>
-            <p> Rhythm & Blues connoissuer. Songwriting novice. Physical Fitness addict. 
+            <p> Rhythm & Blues connoissuer. Songwriter. Physical Fitness addict. 
             Retired pickup basketball player. Combat sports junkie. Trivia fanatic. Part-time Graphic Designer. Food enthusiast.</p>
         </section>
     )
